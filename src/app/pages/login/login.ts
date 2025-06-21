06a7ba0ba80f56a7b45d06ddfc6358014a4f94d7
pedsarg <pedsar@gmail.com>
2025-06-21 11:36:56 -0300

    Pressionar 'Enter' no login, usuario->senha || senha-> login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core'; // Adicione ElementRef aqui
 import { Router } from '@angular/router';
-import { AuthService } from '../../service/auth.service'; 
+import { AuthService } from '../../service/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,33 +17,31 @@ export class Login {
   password = '';
   error = '';
 
+  @ViewChild('passwordInput') passwordInput!: ElementRef<HTMLInputElement>;
+
   constructor(private auth: AuthService, private router: Router) {}
 
-
   login() {
-  if (this.user === 'root' && this.password === 'toor') {
-    sessionStorage.setItem('usuarioId', '1'); // simula login
-    this.router.navigate(['/home']);
-  } else {
-    this.error = 'Usuário ou senha inválidos';
+    if (this.user === 'root' && this.password === 'toor') {
+      sessionStorage.setItem('usuarioId', '1'); // simula login
+      this.router.navigate(['/home']);
+    } else {
+      this.error = 'Usuário ou senha inválidos';
+    }
   }
-}
-
-  // login() {    
-  //   this.error = '';
-  //   this.auth.login(this.user, this.password).subscribe({
-  //     next: (res: any) => {
-  //       // Aqui você pode salvar token, dados do usuário, etc
-  //       this.router.navigate(['/home']);
-  //     },
-  //     error: (err: any) => {
-  //       this.error = 'Usuário ou senha inválidos';
-  //     }
-  //   });
-  // }
 
   logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
+
+focusPasswordInput() {
+  console.log('Tentando focar a senha...');
+  if (this.passwordInput) {
+    console.log('Elemento da senha encontrado!', this.passwordInput.nativeElement);
+    this.passwordInput.nativeElement.focus();
+  } else {
+    console.log('Elemento da senha NÃO encontrado!');
+  }
+}
 }
