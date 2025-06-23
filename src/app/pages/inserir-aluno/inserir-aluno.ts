728518e646b8ac15e7fda5e2f2d5923fc57891de
pedsarg <pedsar@gmail.com>
2025-06-22 23:02:41 -0300

    Cadastrar Aluno - Barra de pesquisa

--- src/app/pages/inserir-aluno/inserir-aluno.ts
+++ src/app/pages/inserir-aluno/inserir-aluno.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../../sidebar/sidebar';
+import { SearchCompassComponent,} from '../../search-compass.component/search-compass.component';
 
 @Component({
   selector: 'app-inserir-aluno',
-  imports: [Sidebar],
+  imports: [Sidebar, SearchCompassComponent],
   templateUrl: './inserir-aluno.html',
   styleUrl: './inserir-aluno.css'
 })
